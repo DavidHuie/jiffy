54433acf4bde6ac95620cc328b2da771c95b7322
David Huie <dahuie@gmail.com>
2014-06-28 16:46:18 -0700

    More tests

--- jiffy_test.go
+++ jiffy_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func TestGetTopic(t *testing.T) {
-	topic := GetTopic("test_topic")
-	if topic.Name != "test_topic" {
+	topic := GetTopic("test_topic1")
+	if topic.Name != "test_topic1" {
 		t.Errorf("Invalid name")
 	}
 	if len(topic.Subscriptions) != 0 {
@@ -18,14 +18,14 @@ func TestGetTopic(t *testing.T) {
 	}
 	// Getting the topic again should return the exact
 	// same topic.
-	topic2 := GetTopic("test_topic")
+	topic2 := GetTopic("test_topic1")
 	if topic.uuid != topic2.uuid {
 		t.Errorf("Topics should be equal")
 	}
 }
 
 func TestTopicFetchData(t *testing.T) {
-	topic := GetTopic("test_topic")
+	topic := GetTopic("test_topic2")
 	message1 := NewMessage("test-name1", "my message1")
 	message2 := NewMessage("test-name2", "my message2")
 	topic.Record(message1, time.Minute)
@@ -41,12 +41,35 @@ func TestTopicFetchData(t *testing.T) {
 }
 
 func TestSubscriptionResponse(t *testing.T) {
-	topic := GetTopic("test_topic")
+	topic := GetTopic("test_topic3")
 	message := NewMessage("test-message", "my message")
 	sub1 := topic.GetSubscription("sub1", time.Minute)
 	sub2 := topic.GetSubscription("sub2", time.Minute)
 
 	topic.Publish(message)
+
+	msg1 := <-sub1.Response
+	msg2 := <-sub2.Response
+
+	if (msg1 != message) || (msg2 != message) {
+		t.Errorf("Invalid messages returned to subscribers")
+	}
+}
+
+func TestTopicRecordAndPublish(t *testing.T) {
+	topic := GetTopic("test_topic4")
+	sub1 := topic.GetSubscription("sub1", time.Minute)
+	sub2 := topic.GetSubscription("sub2", time.Minute)
+	message := NewMessage("test-name1", "my message1")
+	topic.RecordAndPublish(message, time.Minute)
+
+	messages := topic.FetchData()
+	if len(messages) != 1 {
+		t.Errorf("Invalid number of messages returned: %v", len(messages))
+	}
+	if messages[0] != message {
+		t.Errorf("Invalid message returned")
+	}
 
 	msg1 := <-sub1.Response
 	msg2 := <-sub2.Response
