483a17410308629c6614f564e4416513e20483d6
David Huie <dahuie@gmail.com>
2014-06-29 16:43:48 -0700

    Return an error in publish

--- subscription.go
+++ subscription.go
@@ -11,6 +11,7 @@ var (
 	// The wait before timing out a publish.
 	PublishTimeout      = 10 * time.Minute
 	ExpiredSubscription = errors.New("Subscription is expired")
+	PublishTimedOut     = errors.New("Publish timed out")
 )
 
 type Subscription struct {
@@ -33,11 +34,13 @@ func NewSubscription(name string, topic *Topic, ttl time.Duration) *Subscription
 }
 
 // Publishes a message to the subscription.
-func (subscription *Subscription) Publish(message *Message) {
+func (subscription *Subscription) Publish(message *Message) error {
 	ticker := time.NewTicker(PublishTimeout)
 	select {
 	case subscription.Response <- message:
+		return nil
 	case <-ticker.C:
+		return PublishTimedOut
 	}
 }
 
